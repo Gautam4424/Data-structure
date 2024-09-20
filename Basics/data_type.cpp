4e1dada210194397bdc23225aac4f67308cc6f14
Gautam Sachdeva <gautamsachdeva201@gmail.com>
2024-09-21 00:49:37 +0530

    Data types

---
+++ Basics/data_type.cpp
@@ -0,0 +1,24 @@
+#include <iostream>>
+using namespace std;
+
+
+int main() {
+
+
+    // Premitive datatype
+
+    int a =10 ;//store the exact integer value 
+    short b =10 ;  // same as int but the range is low
+    long c = 11234345 ; // same as int the range in higher that int
+    long long n = 2345678; // smae as int r=ange is higher that long
+
+    float f= 3.2 ; // store all the decimal numbers 
+    double h = 12345.34456 ;  // same as float the range is higher 
+
+    char name = 'c' ;//store the charancter in  variable
+
+    bool flag =true  ; // store variable value as true or false only 
+
+
+    //derived  like array ,vector ,  objects (js) will see more furthur
+}
